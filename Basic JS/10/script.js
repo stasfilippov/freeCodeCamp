834677c3196e28c006d05dc75d4063942acb5cc9
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-02-27 23:45:48 +0300

    add: const

--- Basic JS/10/script.js
+++ Basic JS/10/script.js
@@ -1,9 +1,4 @@
-// Only change code below this line
-var a = 5;
-var b = 10;
-var c = 'I am a';
-// Only change code above this line
-
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
