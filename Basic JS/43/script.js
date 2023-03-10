61deb8039206f7d4da2dc04ac5d2f3ce695719ea
stasfilippov <kron888999@gmail.com>
2023-03-10 22:53:08 +0300

    method pop()

---
+++ Basic JS/43/script.js
@@ -0,0 +1,7 @@
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop(); 
+// Only change code below this line
+
+console.log(myArray);
+console.log(removedFromMyArray);
