209aa71988b5fa0664601c7980c78647a7008878
stasfilippov <kron888999@gmail.com>
2023-03-11 11:12:36 +0300

    add: заполнение массива с помощью push

---
+++ Basic JS/46/script.js
@@ -0,0 +1,10 @@
+const myList = [];
+
+myList.push(["Chocolate Bar", 15]);
+myList.push(["Milk", 1]);
+myList.push(["Cheese", 2]);
+myList.push(["Coffee", 3]);
+myList.push(["Bread", 1]);
+
+
+console.log(myList);
