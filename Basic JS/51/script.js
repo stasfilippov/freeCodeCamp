d33cc4c69db4b7e23bfe85134fd706b54a5ecb49
stasfilippov <kron888999@gmail.com>
2023-03-12 11:13:39 +0300

    add: global and local vars

--- Basic JS/51/script.js
+++ Basic JS/51/script.js
@@ -1,5 +1,24 @@
-function timesFive (num) {
-    return num * 5;
-}
+'use strict';
 
-console.log (timesFive(0));
+// Declare the myGlobal variable below this line
+  let myGlobal = 10;
+
+
+function fun1() {
+    // Assign 5 to oopsGlobal here
+    oopsGlobal = 5;
+  
+}
+  
+  // Only change code above this line
+  
+function fun2() {
+    let output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
---
+++ Basic JS/52/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function myLocalScope() {
+  // Only change code below this line
+  let myVar; 
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
