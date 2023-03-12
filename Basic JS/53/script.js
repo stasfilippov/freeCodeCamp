2a482780e67c7e04f711cba7e502c4891b34c38e
stasfilippov <kron888999@gmail.com>
2023-03-12 11:19:33 +0300

    add: переопределение переменной

---
+++ Basic JS/53/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = 'sweater'
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
