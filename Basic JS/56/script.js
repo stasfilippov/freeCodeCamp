26a259bd09d36a1d3f6a457822cb458cd8984879
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-03-14 18:16:59 +0300

    add: grid-area

--- Basic JS/56/script.js
+++ Basic JS/56/script.js
@@ -9,7 +9,3 @@ function nextInLine(arr, item) {
 // Setup
 let testArr = [1, 2, 3, 4, 5];
 
-// Display code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
