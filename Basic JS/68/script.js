713a6cfb3cac13f8a7862c7ae285ab34b233a4ab
stasfilippov <kron888999@gmail.com>
2023-03-17 22:17:11 +0300

    add: operator ||

---
+++ Basic JS/68/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val < 10) {
+    return 'Outside';
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
