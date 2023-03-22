49b346bdced2b5dd9d631de23208214d8d4babb6
stasfilippov <kron888999@gmail.com>
2023-03-22 22:39:27 +0300

    add: добавление нового объекта в массив

---
+++ Basic JS/91/script.js
@@ -0,0 +1,22 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "КиШ",
+    "title": "Прыгнуть со скалы",
+    "release_year": 1990,
+    "formats": [
+      "CD",
+      "LP"
+    ]
+  }
+];
